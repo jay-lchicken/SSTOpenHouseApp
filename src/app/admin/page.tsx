3300142bc3ed869576d0d3356e28d09f401582d7
jay_lchicken <joyjay606@gmail.com>
2026-03-19 17:49:11 +0800

    add RSVP page to display collected email responses for Open House 2026

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,26 @@
+import pool from "@/lib/db";
+
+export default async function RSVPPage() {
+    const RSVPResponses = await pool.query("SELECT email FROM openhouse_rsvp");
+    const emails = RSVPResponses.rows.map(row => row.email);
+    console.log("RSVP Emails:", emails);
+
+  return (
+      <div className="app-domain rsvp-page">
+        <div className="texts">
+          <h1>
+              <span>RSVP responses for Open House 2026</span>
+          </h1>
+          <div>
+              {emails.map((email, i) => (
+                  <div key={i}>
+                      <p>{email}</p>
+                  </div>
+              ))}
+          </div>
+
+
+        </div>
+      </div>
+  );
+}
